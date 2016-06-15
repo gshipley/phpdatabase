e441abf25b973be40ac9d9681bfa64627bab5dc0
Grant Shipley <gshipley@gmail.com>
2016-06-15 18:23:34 -0400

    fixed typo

--- dbtest.php
+++ dbtest.php
@@ -9,7 +9,7 @@ $connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
 if ($connection->connect_errno) {
     printf("Connect failed: %s\n", $mysqli->connect_error);
     exit();
-} esle {
+} else {
     printf("Connected to the database");
 $connection->close();
 ?>
