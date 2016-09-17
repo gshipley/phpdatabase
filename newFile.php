ba7009d5fe613a60a5fd74ac8b2d8d5c07f9f2b1
charlesgiovanniello <charles.giovanniello@gmail.com>
2016-09-16 23:14:19 -0400

    Update newFile.php

--- newFile.php
+++ newFile.php
@@ -9,6 +9,11 @@ if ($connection->connect_errno) {
     printf("Connect failed: %s\n", $mysqli->connect_error);
     exit();
 } else {
+    echo "Host: "$dbhost;
+    echo "Port: "$dbport;
+    echo "user: "$dbuser;
+    echo "pwd: "$dbpwd;
+    echo "dbname: "$dbname;
     printf("Connected to the database");
 }
 $connection->close();
