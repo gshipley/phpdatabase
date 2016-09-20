2c5440ba3b5eb986cab8ceaf000845a92f20c924
charlesgiovanniello <charles.giovanniello@gmail.com>
2016-09-20 18:27:03 -0400

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,4 +1,5 @@
 
+<!--
 <!DOCTYPE html>
 <html lang="en" xmlns="http://www.w3.org/1999/html">
 <head>
@@ -8,7 +9,7 @@
 
 	<link rel="stylesheet" href="tester/bootstrap-switch.css"/>
 	<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <!--<link href="bootstrap/css/bootstrap-switch.css" rel="stylesheet">-->
+    <!--<link href="bootstrap/css/bootstrap-switch.css" rel="stylesheet">
 	<link href="bootstrap/css/docs.min.css" rel="stylesheet">
 	<link href="bootstrap/css/main.css" rel="stylesheet">
 	
@@ -174,6 +175,7 @@
 </head>
 
 <body>
+-->
 <?php
 $dbhost = getenv("MYSQL_SERVICE_HOST");
 $dbport = getenv("MYSQL_SERVICE_PORT");
@@ -212,16 +214,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 }
 ?>
+<!--
 <div class="main">
 <div class="leftCol">
 
 
-<!--Service Desk Metrics-->
+
 <div class="leftCol">
 <div class="row" id="rcorner_main">
 	<h1 class="h3">Bump Test <a style="color:grey" ></a></h1>
 
-	<form id="helpdeskSlas" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
+	<form id="helpdeskSlas" action="" method="POST">
 	<div class="col-sm-6 col-lg-6"><label for= "edit-submitted-first-name">Gender</label>
 		<div class="dropdown" >
 			<select name="gender" class="form-control">
@@ -269,3 +272,4 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 </body>
 </html>
+-->
