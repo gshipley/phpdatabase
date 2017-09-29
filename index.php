0ffa992134cec5e52041b6832d667400b0ab1f35
gshipley <gshipley@gmail.com>
2017-09-29 08:14:45 -0400

    test

--- index.php
+++ index.php
@@ -5,5 +5,5 @@ echo "<br>To test the database, hit the dbtest.php URL";
 
 
 echo "<br><br>This assumes that you have the correct env variables set";
-
+echo "<BR><BR>The environment variables required are databaseuser, databasepassword, and databasenmae.";
 
